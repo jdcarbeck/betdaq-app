e99a1907f9fce4219e6190c78aaf10fbcb4ebcb3
jdcarbeck <jdcarbeck@gmail.com>
2019-02-20 14:43:10 +0000

    add outline for work

---
+++ betdaqAPI/readOnly/readOnly.py
@@ -0,0 +1,16 @@
+from betdaqAPI.baseclient import BaseClient
+
+#all the readonly go in here
+#this clase baseclient and use the readonly part of it
+
+#implement testing (BONUS!!)
+
+class: ReadOnly
+    def __init__(self, username, password):
+    #create a client for the method below to use
+
+    #create a function for every readonly in API doc 25-36 and check parameters
+    #11 functions to be implemented TODO: Tuesday
+    
+    def listTopLevelEvents(self):
+        #check parameters and throw errors appropriate
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-from betdaqAPI.baseclient import BaseClient
+from betdaqAPI.readOnly import ReadOnly
 import config
 
 def main():
     # creates a base to use the readonly_client and eventually secure client
     base = BaseClient(config.username, config.password)
     #sample of creating a call, requesting top events from api
-    print(base.readonly_client.service.ListTopLevelEvents())
+
+    print(readonly.ListTopLevelEvents())
+
+    # Ideally this is how we call functions
+    # base.ListTopLevelEvents()
 
 if __name__ == "__main__":
     main()
