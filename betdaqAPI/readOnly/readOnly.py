ebc137acd77e518ae0739e7d84a69489368ad13d
barryb6 <47220113+barryb6@users.noreply.github.com>
2019-03-10 15:30:54 +0000

    Added attempts at ReadOnly methods 5-8

--- betdaqAPI/readOnly/readOnly.py
+++ betdaqAPI/readOnly/readOnly.py
@@ -10,11 +10,24 @@ class ReadOnly(BaseClient):
     def listTopLevelEvents(self):
         return self.client.service.ListTopLevelEvents()
 
-    #create a client for the method below to use
+
+#TODO: BRIAN 5-8
+    def listSelectionsChangedSince(self):
+        return self.client.service.ListSelectionsChangedSince()
+
+    def listMarketWithdrawalHistory(self, long Market):
+        return self.client.service.ListMarketWithdrawalHistory()
+
+    def getPrices(self, String currency, MoneyAmount thresholdAmount, int numberForPricesRequired,int numberAgainstPricesRequired):
+        return self.client.service.GetPrices()
+
+    def getOddsLadder(self, PriceFormat priceFormat):
+        return self.client.service.GetOddsLadder()
+
+      #create a client for the method below to use
 
     #create a function for every readonly in API doc 25-36 and check parameters
     #11 functions to be implemented TODO: THURSDAY
     #TODO: CIAN 8-11
-    #TODO: BRIAN 5-8
     #TODO: JOESEPH 1-4
     
