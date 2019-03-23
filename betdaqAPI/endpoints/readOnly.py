663d60dbe62b7a53ed3eec9afcb709ea57fcf446
JackDCollins <collinj6@tcd.ie>
2019-03-23 14:49:18 +0000

    added get Market info method

--- betdaqAPI/endpoints/readOnly.py
+++ betdaqAPI/endpoints/readOnly.py
@@ -15,4 +15,7 @@ class Endpoint(BaseEndpoint):
         def get_event_sub_tree_no_selections(self, event_handle, want_direct_descendents_only=False,  want_play_markets=False):
             return self.request("GetEventSubTreeNoSelections","GetEventSubTreeNoSelectionsRequest",EventClassifierIds=event_handle,_WantDirectDescendentsOnly=want_direct_descendents_only,_WantPlayMarkets=want_play_markets)
 
+        def get_market_information(self, market_ids=False):
+            return self.request("GetMarketInformation","GetMarketInformationRequest",MarketIds=market_ids)
+        
     
