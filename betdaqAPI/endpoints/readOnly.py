e242689cc9312a0f414a9fdbca5924d55128d26e
JackDCollins <collinj6@tcd.ie>
2019-03-23 14:43:40 +0000

    added eventsubtree methods

--- betdaqAPI/endpoints/readOnly.py
+++ betdaqAPI/endpoints/readOnly.py
@@ -3,12 +3,16 @@ from betdaqAPI.enums import PriceFormat
 
 class Endpoint(BaseEndpoint):
 
-    def list_top_level_events(self, want_play_markets=None):
-        """
-        Get list of sports and their IDs.
-        :param want_play_markets: whether to return play or real markets, None is False
-        :return: all sports available
-        """
-        return self.request('ListTopLevelEvents', 'ListTopLevelEventsRequest', _WantPlayMarkets=want_play_markets)
+
+        def list_top_level_events(self, want_play_markets=False):
+        
+            return self.request("ListTopLevelEvents", "ListTopLevelEventsRequest", _WantPlayMarkets=want_play_markets)
+        
+        def get_event_sub_tree_with_selections(self, event_handle, want_play_markets=False):
+
+            return self.request("GetEventSubTreeWithSelections", "GetEventSubTreeWithSelectionsRequest", EventClassifierIds=event_handle,_WantPlayMarkets=want_play_markets)
+
+        def get_event_sub_tree_no_selections(self, event_handle, want_direct_descendents_only=False,  want_play_markets=False):
+            return self.request("GetEventSubTreeNoSelections","GetEventSubTreeNoSelectionsRequest",EventClassifierIds=event_handle,_WantDirectDescendentsOnly=want_direct_descendents_only,_WantPlayMarkets=want_play_markets)
 
     
