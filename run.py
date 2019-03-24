186fed48ffc40c472760f423b010330cb31a3f90
JackDCollins <collinj6@tcd.ie>
2019-03-24 10:08:15 +0000

    got prices on each dog for the strategy

--- run.py
+++ run.py
@@ -1,51 +1,69 @@
 from betdaqAPI.baseclient import BaseClient
-
+from betdaqAPI.enums import MarketStatus
+from betdaqAPI.enums import MarketType
 
 import config
 
 def main():
-    runStrat()
+    GreyHoundRacingID = 100008
+    client = BaseClient(config.username, config.password)
+    runStrat(client,GreyHoundRacingID)
     
 
 
-def runStrat():
-    GreyHoundRacingID = 100008
+def runStrat(client,id):
+    
+    _event_Classifier = "EventClassifiers"
+    _markets = "Markets"
+    _market_Type = "Type"
+    _Id = "Id"
 
-    client = BaseClient(config.username, config.password)
-    resp = client.readonly.get_event_sub_tree_no_selections([GreyHoundRacingID])
-    a = resp.get("EventClassifiers")
+
+    #Get The Sport's Substree
+    resp = client.readonly.get_event_sub_tree_no_selections(id)
+    a = resp.get(_event_Classifier)
     b = a[0]
-    z = b.get("EventClassifiers")
+    z = b.get(_event_Classifier)
     print(type(z))
     x = z[0]
     print(type(x))
-    events= x.get(("EventClassifiers"))
+    events= x.get(_event_Classifier)
     for i in events:
-        for u in i.get("EventClassifiers"):
-            for r in u.get("Markets"):
-                print("\n\n==================")
-                y = client.readonly.get_prices([r.get("Id")])
+        for u in i.get(_event_Classifier):
+            for r in u.get(_markets):
+                if r.get(_market_Type) is MarketType.Win.value:
+                    y = client.readonly.get_prices([r.get(_Id)])
+                    for q in y.get("MarketPrices"):
+                        for j in q.get("Selections"):
+                            print("\n\n",j)
+                            print(j.get("Name"))
+                            
 
-                for q in y.get("MarketPrices"):
-                    for j in q.get("Selections"):
-                        print(j.get("Name"))
-                
-                
+def run2(client, id):
 
-           
+    _event_Classifier = "EventClassifiers"
+    _markets = "Markets"
+    _market_Type = "Type"
+    _Id = "Id"
 
-    #print(v.get("ParentId"))
-
-    #for element in v :
-        #print("\n\n ============================= \n \n")
-        #print(element)
-
-        
-
-
-    #for element in resp:
-        #print(element[0])
-        #print("=")
+    #Get The Sport's Substree
+    resp = client.readonly.get_event_sub_tree_no_selections(id)
+    a = resp.get(_event_Classifier)
+    b = a[0]
+    z = b.get(_event_Classifier)
+    print(type(z))
+    x = z[0]
+    print(type(x))
+    events = x.get(_event_Classifier)
+    for i in events:
+        for u in i.get(_event_Classifier):
+            for r in u.get(_markets):
+                if r.get(_market_Type) is MarketType.Win.value:
+                    y = client.readonly.get_prices([r.get(_Id)])
+                    for q in y.get("MarketPrices"):
+                        for j in q.get("Selections"):
+                            print("\n\n", j)
+                            print(j.get("Name"))
 
 if __name__ == "__main__":
     main()
