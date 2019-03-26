f60ea608c9984624aab147ff8a42c352e9105f83
JackDCollins <collinj6@tcd.ie>
2019-03-26 13:49:14 +0000

    Continued work on strategy- tidying everything up

--- run.py
+++ run.py
@@ -3,13 +3,57 @@ from betdaqAPI.enums import MarketStatus
 from betdaqAPI.enums import MarketType
 
 import config
+import time
 
 def main():
     GreyHoundRacingID = 100008
     client = BaseClient(config.username, config.password)
     # timeAndMarket(client, GreyHoundRacingID)
-    mids = getSportMarketsByType(client,GreyHoundRacingID,MarketType.Win)
-    getApiTime(client)
+    runStart(client,GreyHoundRacingID)
+    # runOrderWithMinBal(client,100)
+    # print(getGBPAvailableFunds(client))
+    # mids = getSportMarketsByType(client, GreyHoundRacingID, MarketType.Win)    
+
+def runStart(client,SportId):
+    #Getting Sport Markets
+    print("##### == ", "Getting Sport Markets", " == #####")
+    mids = getSportMarketsByType(client, SportId, MarketType.Win)
+    print("##### == ", "Market Collection Complete", " == #####")
+    print("##### == ", "Checking Market Time", " == #####")
+    currentTime = getApiTime(client)
+    print("##### == ", "Market Time is ",currentTime, " == #####")
+    print("##### == ", "Getting Market Times", " == #####")
+    timeDict = getMarketTimes(client, mids)
+    print("##### == ", "Found Market Times", " == #####")
+    print(timeDict)
+
+
+    while(True):
+        time.sleep(10)
+        print(getApiTime(client))
+
+
+def runOrderWithMinBal(client,minBal,order):
+    return 0
+
+def activeOrderForID(client, id):
+
+    return True
+
+def getGBPAvailableFunds(client):
+    resp = client.secure.get_account_balances()
+    return resp.get("AvailableFunds")
+
+
+def split(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
 
 #Submit Client, Sport ID and recieve all market ID's in return
 def getSportMarketsByType(client,sportID,marketType):
@@ -37,7 +81,7 @@ def getSportMarketsByType(client,sportID,marketType):
     #gets the subtree
     daily_unwrapped = getNextLevel(sport_unwrapped,_event_Classifier)
 
-    #Gets all the event days
+    #Gets all the Meets/Event Days
     meets_unwrapped = getNextLevel(daily_unwrapped,_event_Classifier)
 
     for meet in meets_unwrapped:
@@ -46,7 +90,7 @@ def getSportMarketsByType(client,sportID,marketType):
             for market in race.get(_markets):
                 mar_type = MarketType(market.get(_market_Type))
                 if mar_type is marketType:
-                    m_id = market.get("Id")
+                    m_id = market.get(_Id)
                     market_ids.append(m_id)
 
     return market_ids
@@ -93,10 +137,24 @@ def timeAndMarket(client, id):
                     # print(race_name," time is ",market.get(_time), " for market type =  ", MarketType(market.get(_market_Type)), " ID = ",market.get("Id"))
                     idz = market.get("Id")
                     market_ids.append(idz)
-    
-    print(market_ids)
-    print(client.readonly.get_market_information(market_ids))
-    print(client.readonly.get_prices(market_ids))
+
+
+def getMarketTimes(client, mids):
+    _market_Prices = "MarketPrices"
+    markets = {}
+    mids_list = split(mids,2)
+    for mids in mids_list:
+        mids_resp = client.readonly.get_prices(mids)
+        a = mids_resp.get(_market_Prices)
+        for e in a:
+            id = e.get("Id")
+            startTime = e.get("StartTime")
+            markets[startTime] = id
+    print(markets)
+    dict(sorted(markets.items()))
+    return markets
+
+
 
 if __name__ == "__main__":
     main()
